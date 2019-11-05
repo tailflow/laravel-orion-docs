2d11b5a77c8c7593ef47f1bf801a3748c24d4c20
Aleksei Zarubin <alex@zarubin.co>
2019-11-05 18:34:18 +0100

    feature: add favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,17 +2,20 @@ module.exports = {
     base: '/docs/',
     title: 'Laravel Orion',
     description: 'The easiest way to create REST API with Laravel',
+    head: [
+        ['link', { rel: "shortcut icon", href: "/favicon.ico" }],
+    ],
     themeConfig: {
         repo: 'laravel-orion/laravel-orion',
         docsRepo: 'laravel-orion/docs',
         docsDir: 'docs',
         editLinks: true,
         nav: [
-          { text: 'Home', link: '/' },
-          { text: 'Guide', link: '/guide/' },
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
         ],
         sidebar: {
-            '/guide/' : [
+            '/guide/': [
                 {
                     title: 'Guide',
                     collapsable: false,
